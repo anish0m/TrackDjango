67bbac7daad2a281dd58f60b442a33be209eea65
anish0m <mohsina.tabassum.rifa@gmail.com>
2024-12-17 17:57:52 +0600

    Debug Django Application in VSCode

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def calculate():
+    x = 1
+    y = 2
+
+    return x
+
 def say_hello(request):
     # return HttpResponse('Hello World')
+    # x = 1
+    # y = 2
+    # x = calculate()
     return render(request, 'hello.html', { 'name': 'anish0m' })
